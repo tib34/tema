078ee7821f45e68be5863313425572d68f5a8e99
tib34 <94251836+tib34@users.noreply.github.com>
2024-01-29 20:43:47 +0100

    Add files via upload

--- test_python.py
+++ test_python.py
@@ -1,8 +1,8 @@
 someNumber = 10
 
-if(someNumber >= 0):
+if(someNumber == 0):
     print("Over zerro")
-elif(someNumber <0):
+elif(someNumber > 0):
     print("Bellow zerro")
 else:
     print("zerro")
